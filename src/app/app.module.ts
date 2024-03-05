2942bf0a46f3cad0efb30ffa563833b8bf887b1c
erray <a.erray@itroadgroup.com>
2024-03-05 18:10:05 +0100

    module is updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,61 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SinginComponent } from './components/singin/singin.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpRequestInterceptor } from './_helpers/http.interceptor';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostComponent } from './components/post/post.component';
+import { LeftBarComponent } from './components/bar/left-bar/left-bar.component';
+import { RightBarComponent } from './components/bar/right-bar/right-bar.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
+import { CreateCommentComponent } from './components/create-comment/create-comment.component';
+import { DisplayCommentComponent } from './components/display-comment/display-comment.component';
+import { LikeComponent } from './components/like/like.component';
+import { AngularFireModule } from '@angular/fire/compat';
+// import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { environment } from '../environments/environment';
+import { SidbarComponent } from './components/dashboard/sidbar/sidbar.component';
+import { UsersComponent } from './components/dashboard/users/users.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SinginComponent,
+    RegisterComponent,
+    HomeComponent,
+    NavbarComponent,
+    PostComponent,
+    LeftBarComponent,
+    RightBarComponent,
+    CreatePostComponent,
+    CreateCommentComponent,
+    DisplayCommentComponent,
+    LikeComponent,
+    SidbarComponent,
+    UsersComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    // AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpRequestInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
