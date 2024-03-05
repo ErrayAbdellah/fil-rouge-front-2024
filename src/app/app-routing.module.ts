ebfb32e6442a91b4802668cba77e1c05f940a70a
erray <a.erray@itroadgroup.com>
2024-03-05 18:09:33 +0100

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SinginComponent } from './components/singin/singin.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { authGuard } from './_guards/authGuard';
+import { UsersComponent } from './components/dashboard/users/users.component';
+import { adminGuard } from './_guards/admin.guard';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '', pathMatch: 'full'},
+  { path: 'dashboard-users' , component: UsersComponent, canActivate: [adminGuard] },
+  { path: 'profile' , component: ProfileComponent ,canActivate: [authGuard]},
+  { path: 'login', component: SinginComponent  },
+  { path: 'register', component: RegisterComponent },
+  { path:"" , component : HomeComponent, canActivate: [authGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
