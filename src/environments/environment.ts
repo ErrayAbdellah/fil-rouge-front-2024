68f9abcc1798130c118dfe1f25a877f217ffd998
erray <a.erray@itroadgroup.com>
2024-03-22 18:24:23 +0000

    integrate firebase in angular

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyDIRySInwUN8T0AFZ_wJONVV5eLJEd7Ctg",
+        authDomain: "fil-rouge-ba110.firebaseapp.com",
+        projectId: "fil-rouge-ba110",
+        storageBucket: "fil-rouge-ba110.appspot.com",
+        messagingSenderId: "1078013117493",
+        appId: "1:1078013117493:web:9540fc51bf8a1acb149d7c"
+      }
+};
